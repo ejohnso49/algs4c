6d4806adab7903cf58fc5d329ba068fdc217298a
Eric Johnson <eric.j.johnson22@gmail.com>
2018-08-19 21:17:32 -0700

    Update test_stack_init_free
     - Rename test function
     - Add checks for successful heap allocation of stack and stack->data
     - Add call to free stack to fix memory leak
     - Add check that stack memory has been freed

--- array_structures/unit_test/test_stack.c
+++ array_structures/unit_test/test_stack.c
@@ -5,22 +5,28 @@
 #include "../stack.h"
 #include "../../unity/src/unity.h"
 
-void test_stack_init(void) {
+void test_stack_init_free(void) {
     stack_t *stack;
     unsigned int test_N = 50;
     unsigned int test_init_val = 0;
     stack_init(&stack, test_N, sizeof(unsigned int));
 
+    TEST_ASSERT_NOT_NULL(stack);
+    TEST_ASSERT_NOT_NULL(stack->data);
     TEST_ASSERT_EQUAL_UINT(test_N, stack->N);
     TEST_ASSERT_EACH_EQUAL_MEMORY(&test_init_val, stack->data, sizeof(unsigned int), test_N);
     TEST_ASSERT_EQUAL_UINT(0, stack->size);
     TEST_ASSERT_EQUAL_UINT(sizeof(unsigned int), stack->elem_size);
+
+    stack_free(&stack);
+
+    TEST_ASSERT_NULL(stack);
 }
 
 int main(void) {
     UNITY_BEGIN();
 
-    RUN_TEST(test_stack_init);
+    RUN_TEST(test_stack_init_free);
 
     return UNITY_END();
 }
