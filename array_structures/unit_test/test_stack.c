bd3e3a14695fe613b9662df83cb6998baab1c4fc
Eric Johnson <eric.j.johnson22@gmail.com>
2018-08-20 23:54:16 -0700

    Add test_stack_push_pop to test_stack

--- array_structures/unit_test/test_stack.c
+++ array_structures/unit_test/test_stack.c
@@ -23,10 +23,41 @@ void test_stack_init_free(void) {
     TEST_ASSERT_NULL(stack);
 }
 
+void test_stack_push_pop(void) {
+    stack_t *stack = NULL;
+    unsigned int test_N = 50;
+    unsigned int result = 0;
+    unsigned int empty_pop;
+
+    stack_init(&stack, test_N, sizeof(unsigned int));
+    for (unsigned int i = 0; i < test_N; i++) {
+        stack_push(stack, (void *) &i);
+    }
+
+    TEST_ASSERT_EQUAL_UINT(test_N, stack->N);
+    TEST_ASSERT_EQUAL_UINT(test_N, stack->size);
+
+
+    for (int i = test_N - 1; i >= 0; i--) {
+        stack_pop(stack, &result);
+        TEST_ASSERT_EQUAL_UINT(i, result);
+        TEST_ASSERT_EQUAL_UINT(i, stack->size);
+    }
+    TEST_ASSERT_EQUAL_UINT(0, stack->size);
+
+    empty_pop = result;
+    stack_pop(stack, &empty_pop);
+    TEST_ASSERT_EQUAL_UINT(result, empty_pop);
+
+    stack_free(&stack);
+    TEST_ASSERT_NULL(stack);
+}
+
 int main(void) {
     UNITY_BEGIN();
 
     RUN_TEST(test_stack_init_free);
+    RUN_TEST(test_stack_push_pop);
 
     return UNITY_END();
 }
